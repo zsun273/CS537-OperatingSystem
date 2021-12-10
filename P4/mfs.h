dd9d002c99fdca87aadb0dcb5c4cf0707f374875
Leo Sun <zsun273@wisc.edu>
2021-12-10 00:54:12 -0600

    initialization in progress

--- P4/mfs.h
+++ P4/mfs.h
@@ -17,6 +17,47 @@ typedef struct __MFS_DirEnt_t {
     int  inum;      // inode number of entry (-1 means entry not used)
 } MFS_DirEnt_t;
 
+//struct for checkpoint region, pointing to 256 pieces of inode map
+typedef struct __MFS_checkpoint_t {
+    int imap[256];  // point to inode map pieces
+    int endOfLog;
+} MFS_checkpoint_t;
+
+//struct for inode map
+typedef struct __MFS_imap_t {
+    int inodeArr[16]; // an array indexed by inode number
+} MFS_imap_t;
+
+//struct for inode
+typedef struct __MFS_inode_t {
+    int type;
+    int size;
+    int blockArr[14]; // point to 4KB block
+} MFS_inode_t;
+
+typedef enum __MFS_lib_t {
+    INIT,
+    LOOKUP,
+    STAT,
+    WRITE,
+    READ,
+    CREAT,
+    UNLINK,
+    SHUTDOWN
+} MFS_lib_t;
+
+// message struct to be sent between server and client
+typedef  struct __MFS_Msg_t {
+    char name[28]; //name of the directory
+    char buffer[4096];
+    int inum;
+    int block;
+    int type;
+    int returnNum;
+    int pinum;
+    MFS_Stat_t stat;
+    MFS_lib_t lib;
+} MFS_Msg_t;
 
 int MFS_Init(char *hostname, int port);
 int MFS_Lookup(int pinum, char *name);
--- P4/server.c
+++ P4/server.c
@@ -1,28 +1,108 @@
 #include <stdio.h>
 #include "udp.h"
+#include "mfs.h"
 
-#define BUFFER_SIZE (1000)
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+
+#define BUFFER_SIZE (4096)
+
+int init_image(char *name); // function to initialize the file image
+int load_mem();             // load check point, inode map into memory
+
+
+char name[256]; // TODO: not sure about this size
+MFS_checkpoint_t CR;
+MFS_imap_t imap;
 
 // server code
 int main(int argc, char *argv[]) {
-    int sd = UDP_Open(10000);  // use a port-number to open
+
+    if (argc != 3){
+        fprintf(stderr, "Usage: server <port> <file image>\n");
+        exit(0);
+    }
+    int port_num = atoi(argv[1]);
+    sprintf(name, argv[2]);       // assign file image to name
+
+    int sd = UDP_Open(port_num);  // use a port-number to open
     assert(sd > -1);
+
+    // initialize the image file
+    init_image(name);
+    load_mem();
+
     while (1) {
-	struct sockaddr_in addr;
-	char message[BUFFER_SIZE]; // TODO: this need to be changed to struct
-	printf("server:: waiting...\n");
-	int rc = UDP_Read(sd, &addr, message, BUFFER_SIZE);
-	printf("server:: read message [size:%d contents:(%s)]\n", rc, message);
-	if (rc > 0) {
-            char reply[BUFFER_SIZE];
-            sprintf(reply, "goodbye world");
-            rc = UDP_Write(sd, &addr, reply, BUFFER_SIZE);
-	    printf("server:: reply\n");
-	} 
+	    struct sockaddr_in addr;
+        char message[sizeof(MFS_Msg_t)];
+        printf("server:: waiting...\n");
+        int rc = UDP_Read(sd, &addr, message, sizeof(MFS_Msg_t));
+        printf("server:: read message [size:%d contents:(%s)]\n", rc, message);
+        if (rc > 0) {
+            char reply[sizeof(MFS_Msg_t)];
+            rc = UDP_Write(sd, &addr, reply, sizeof(MFS_Msg_t));
+            printf("server:: reply\n");
+        }
     }
     return 0; 
 }
     
+int init_image(char* name){
+    int fd = open(name, O_RDWR); // open an existing file with r/w access
 
+    if (fd < 0){                 // filr not exist, create and initialize
+        fd = open(name, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU);
+        // create checkpoint region, initial inode map, create a single root directory with . and ..
+        // first initialize two directory entries
+        MFS_DirEnt_t self_dir;
+        MFS_DirEnt_t parent_dir;
+        self_dir.inum = 0;
+        sprintf(self_dir.name, ".\0");
+        parent_dir.inum = 0;
+        sprintf(parent_dir.name, "..\0")
 
+        // then initialize inode for root directory
+        MFS_inode_t root;
+        root.type = MFS_DIRECTORY;
+        for (int i = 0; i < 14; ++i) {
+            MFS_DirEnt_t dir_entry;
+            sprintf(dir_entry.name, "\0");
+            dir_entry.inum = -1;
+            root.blockArr[i] = dir_entry;
+        }
+        root.blockArr[0] = self_dir;
+        root.blockArr[1] = parent_dir;
+
+        // then initialize the inode map
+        for (int i = 0; i < 16; ++i) {
+            imap.inodeArr[i] = -1;
+        }
+        imap.inodeArr[0] = root;
+
+        // then initialize the checkpoint region
+        for (int i = 0; i < 256; ++i) {
+            CR.imap[i] = -1;
+        }
+        CR.imap[0] = imap;
+        CR.endOfLog = sizeof(CR) + sizeof(imap) + sizeof(root) + sizeof(self_dir) + sizeof(parent_dir);
+
+        lseek(fd, 0, SEEK_SET); // set the file offset to 0
+        write(fd, &CR, sizeof(MFS_checkpoint_t));
+        write(fd, &imap, sizeof(MFS_imap_t));
+        write(fd, &root, sizeof(MFS_inode_t));
+        write(fd, &self_dir, sizeof(MFS_DirEnt_t));
+        write(fd, &parent_dir, sizeof(MFS_DirEnt_t));
+    }
+    else{
+        read(fd, &CR, sizeof(MFS_checkpoint_t));
+    }
+
+    return 0;
+}
+
+int load_mem(){
+    return 0;
+}
  
+
+#pragma clang diagnostic pop
