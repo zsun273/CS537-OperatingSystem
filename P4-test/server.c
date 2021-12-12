287957c4520265c210551a441761cc6a10ecd770
Leo Sun <zsun273@wisc.edu>
2021-12-11 23:42:16 -0600

    add debug info for free test

--- P4-test/server.c
+++ P4-test/server.c
@@ -326,6 +326,8 @@ int sLookup(int pinum, char *name) {
 }
 
 int sCreate(int pinum, int type, char *name) {
+    printf("create starts\n");
+
     if(pinum < 0 || pinum > 4096) return -1;
     if(type != MFS_DIRECTORY && type != MFS_REGULAR_FILE) return -1;
     if(strlen(name) < 1 || strlen(name) >= 28) return -1;
